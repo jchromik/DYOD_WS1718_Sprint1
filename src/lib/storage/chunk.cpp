eb6574f8ae31de606a73b63336a46fd451e9cb64
Jonas Chromik <jonas.chromik@student.hpi.de>
2017-10-21 17:16:16 +0200

    Sprint 1, Step 3: Basic implementation of Chunks.

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -15,26 +15,35 @@
 namespace opossum {
 
 void Chunk::add_column(std::shared_ptr<BaseColumn> column) {
-  // Implementation goes here
+  _columns.push_back(column);
 }
 
 void Chunk::append(std::vector<AllTypeVariant> values) {
-  // Implementation goes here
+  if(values.size() != _columns.size()) {
+    throw std::runtime_error("values do not match columns");
+  }
+  auto val_it = values.begin();
+  auto col_it = _columns.begin();
+  while(val_it != values.end() && col_it !=_columns.end()) {
+    col_it->get()->append(*val_it);
+    ++val_it;
+    ++col_it;
+  }
 }
 
 std::shared_ptr<BaseColumn> Chunk::get_column(ColumnID column_id) const {
-  // Implementation goes here
-  return nullptr;
+  return _columns.at(column_id);
 }
 
 uint16_t Chunk::col_count() const {
-  // Implementation goes here
-  return 0;
+  return _columns.size();
 }
 
 uint32_t Chunk::size() const {
-  // Implementation goes here
-  return 0;
+  if (_columns.size() == 0) {
+    return 0;
+  }
+  return _columns.at(0)->size();
 }
 
 }  // namespace opossum
