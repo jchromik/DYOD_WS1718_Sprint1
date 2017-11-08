f45a589f4b813a2cb7e2240290d11217b491e27f
Jonas Chromik <jonas.chromik@student.hpi.de>
2017-11-08 16:41:26 +0100

    Add tests for (yet) untemplated implementation of FittedAttributeVector

--- src/lib/storage/fitted_attribute_vector.cpp
+++ src/lib/storage/fitted_attribute_vector.cpp
@@ -3,10 +3,16 @@
 namespace opossum {
 
 ValueID FittedAttributeVector::get(const size_t i) const {
+  if(i >= _entries.size()) {
+    throw std::runtime_error("Index out of range");
+  }
   return ValueID{_entries.at(i)};
 }
 
 void FittedAttributeVector::set(const size_t i, const ValueID value_id) {
+  if(i >= _entries.size()) {
+    throw std::runtime_error("Index out of range");
+  }
   _entries.erase(_entries.begin() + i);
   _entries.emplace(_entries.begin() + i, value_id);
 }
--- src/test/storage/fitted_attribute_vector_test.cpp
+++ src/test/storage/fitted_attribute_vector_test.cpp
@@ -17,6 +17,30 @@ TEST_F(StorageFittedAttributeVectorTest, Width) {
   EXPECT_EQ(fav.width(), sizeof(ValueID)*8);
 }
 
-// TODO: more tests
+TEST_F(StorageFittedAttributeVectorTest, SetGet) {
+  fav.set(0, ValueID{2});
+  fav.set(1, ValueID{4});
+  fav.set(2, ValueID{8});
+  fav.set(3, ValueID{16});
+  fav.set(4, ValueID{32});
+  EXPECT_EQ(fav.get(0), ValueID{2});
+  EXPECT_EQ(fav.get(1), ValueID{4});
+  EXPECT_EQ(fav.get(2), ValueID{8});
+  EXPECT_EQ(fav.get(3), ValueID{16});
+  EXPECT_EQ(fav.get(4), ValueID{32});
+}
+
+TEST_F(StorageFittedAttributeVectorTest, SetOutOfRange) {
+  EXPECT_THROW(fav.set(5, ValueID{2}), std::exception);
+}
+
+TEST_F(StorageFittedAttributeVectorTest, GetUninitialized) {
+  // Return default value
+  EXPECT_EQ(fav.get(0), ValueID{0});
+}
+
+TEST_F(StorageFittedAttributeVectorTest, GetOutOfRange) {
+  EXPECT_THROW(fav.get(5), std::exception);
+}
 
 } // namespace opossum
